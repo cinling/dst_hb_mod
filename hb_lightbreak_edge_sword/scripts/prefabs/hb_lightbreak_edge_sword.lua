200acb17b5ced60b95c7b156ef8c6929fbdc0b92
cinling <1105412681@qq.com>
2023-04-05 18:21:51 +0800

    :hammer: 添加根据斩味设定伤害和砍树伤害的处理

--- hb_lightbreak_edge_sword/scripts/prefabs/hb_lightbreak_edge_sword.lua
+++ hb_lightbreak_edge_sword/scripts/prefabs/hb_lightbreak_edge_sword.lua
@@ -1,9 +1,87 @@
+--- 碎光之晓刀
+-- 基础攻击力 55（面板属性）
+-- 耐久度 100
+-- 特点 持有时可以发出微弱的光芒，并且可以发出热量，冬天时可提供一定的热量
+-- 每攻击10次，将会触发一次范围性爆炸伤害（不会着火），伤害值是300
+
+--- 斩味
+-- 颜色   伤害  耐久度     伤害倍率
+-- 紫    76    >=90      1.39
+-- 白    72    70-89     1.32
+-- 蓝    68    50-69     1.25
+-- 绿    61    40-49     1.125
+-- 黄    55    20-39     1
+-- 橙    41    0-19      0.75
+-- 红    27    =0        0.5
+
+--- 参考文件：
+-- spear.lua 长矛
+-- axe.lua 斧头
+
 local assets =
 {
     Asset("ANIM", "anim/hb_lightbreak_edge_sword.zip"),
     Asset("ANIM", "anim/swap_hb_lightbreak_edge_sword.zip"),
     Asset("ATLAS", "images/inventoryimages/hb_lightbreak_edge_sword.xml"),
 }
+
+-- 最大耐久度
+local HB_LIGHTBREAK_EDGE_USES_MAX = 100
+-- 初始化耐久度
+local HB_LIGHTBREAK_EDGE_USES_INIT = 60
+
+-- 斩味
+local HB_SHARPNESS = {
+    PURPLE = {damage = 76, chop = 20},
+    WHITE = {damage = 72, chop = 15},
+    BLUE = {damage = 68, chop = 10},
+    GREEN = {damage = 61, chop = 5},
+    YELLOW = {damage = 55, chop = 2},
+    ORANGE = {damage = 41, chop = 1},
+    RED = {damage = 27, chop = 1},
+}
+
+--- 斩味class
+HbSharpness = {
+    damage = 0,
+    chop = 0
+}
+
+--- 新建一个斩味对象
+-- percent 耐久度百分比
+function HbSharpness:new(
+        percent -- 耐久度百分比
+)
+    if percent >= 0.9 then
+        return HB_SHARPNESS.PURPLE
+    elseif percent >= 0.7 then
+        return HB_SHARPNESS.WHITE
+    elseif percent >= 0.5 then
+        return HB_SHARPNESS.BLUE
+    elseif percent >= 0.4 then
+        return HB_SHARPNESS.GREEN
+    elseif percent >= 0.2 then
+        return HB_SHARPNESS.YELLOW
+    elseif percent > 0 then
+        return HB_SHARPNESS.ORANGE
+    else
+        return HB_SHARPNESS.RED
+    end
+end
+
+
+--- 根据耐久度（斩味）重置属性
+local function ResetPropByUses(inst)
+    local sharpness = HbSharpness:new(inst.components.finiteuses:GetPercent())
+
+    inst.components.weapon:SetDamage(sharpness.damage) -- 设置伤害
+    inst.components.tool:SetAction(ACTIONS.CHOP, sharpness.chop) -- 设置砍树威力
+end
+
+--- 耐久度百分比变化
+local function OnPercentUsedChange(inst, data)
+    ResetPropByUses(inst)
+end
 
 local function onequip(inst, owner)
     --local skin_build = inst:GetSkinBuild()
@@ -36,6 +114,11 @@ local function onunequip(inst, owner)
     owner.DynamicShadow:SetSize(1.3, 0.6)
 end
 
+-- 耐久度用完后
+local function OnFinishedUses(inst)
+    ResetPropByUses(inst)
+end
+
 local function fn()
     local inst = CreateEntity()
 
@@ -64,16 +147,21 @@ local function fn()
         return inst
     end
 
+    -- 武器
     inst:AddComponent("weapon")
-    inst.components.weapon:SetDamage(TUNING.SPEAR_DAMAGE)
+
+    -- 工具
+    inst:AddComponent("tool")
 
     -------
 
+    -- 耐久度
     inst:AddComponent("finiteuses")
-    inst.components.finiteuses:SetMaxUses(TUNING.SPEAR_USES)
-    inst.components.finiteuses:SetUses(TUNING.SPEAR_USES)
-
-    inst.components.finiteuses:SetOnFinished(inst.Remove)
+    inst.components.finiteuses:SetMaxUses(HB_LIGHTBREAK_EDGE_USES_MAX)
+    inst.components.finiteuses:SetUses(HB_LIGHTBREAK_EDGE_USES_INIT)
+    inst.components.finiteuses:SetOnFinished(OnFinishedUses)
+    inst.components.finiteuses:SetConsumption(ACTIONS.CHOP, 1) -- 砍树消耗1耐久度
+    ResetPropByUses(inst)
 
     inst:AddComponent("inspectable")
 
@@ -85,6 +173,9 @@ local function fn()
     inst.components.equippable:SetOnUnequip(onunequip)
 
     MakeHauntableLaunch(inst)
+
+    -- 监听事件
+    inst:ListenForEvent("percentusedchange", OnPercentUsedChange) -- 耐久度变更
 
     return inst
 end
